33585c3392fa579e4742490c1bfb01618c8f3074
KhaepukhaevV <vovan.2098@gmail.com>
2020-11-12 16:16:02 +0300

    add new py file "counter.py"

---
+++ DousonTasks/counter.py
@@ -0,0 +1,9 @@
+print("Программа считалка")
+start = int(input("Введите начало счета: "))
+finish = int(input("Введите конец счета: "))
+interval = int(input("Введите интервал счета: "))
+for i in range(start,finish,interval):
+    print(i, end=" ")
+input("Нажмите Enter для выхода")
+
+
