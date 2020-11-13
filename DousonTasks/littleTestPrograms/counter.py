e483475cb3ee2e18acc55c00a7a58b343b6a3c02
KhaepukhaevV <vovan.2098@gmail.com>
2020-11-13 23:19:43 +0300

    Refactor directories and update code in some programs

--- DousonTasks/littleTestPrograms/counter.py
+++ DousonTasks/littleTestPrograms/counter.py
@@ -2,7 +2,7 @@ print("Программа считалка")
 start = int(input("Введите начало счета: "))
 finish = int(input("Введите конец счета: "))
 interval = int(input("Введите интервал счета: "))
-for i in range(start,finish,interval):
+for i in range(start, finish, interval):
     print(i, end=" ")
 input("Нажмите Enter для выхода")
 
